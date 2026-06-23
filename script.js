267812940107a60a22c79f9854f7c1869b9e2346
Nishant-8040 <111938583+Nishant-8040@users.noreply.github.com>
2026-06-23 02:52:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("JADAUN ELEVATORS WEBSITE LOADED");
+window.addEventListener("scroll", () => {
+const navbar = document.querySelector(".navbar");
+
+if (window.scrollY > 50) {
+navbar.style.boxShadow = "0 4px 20px rgba(0,0,0,0.25)";
+} else {
+navbar.style.boxShadow = "0 2px 10px rgba(0,0,0,0.2)";
+}
+});
+
